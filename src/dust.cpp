366181efa22ea6e2efcbab9a95333d3e129ed104
Zeno Pelgrims <zenopelgrims@gmail.com>
2017-04-02 12:50:53 +0200

    bit of cleanup

--- src/dust.cpp
+++ src/dust.cpp
@@ -3,27 +3,18 @@
 #include <cstring>
 #include <vector>
 
-// TODO
-
-/* to compile and kick on my machine
-g++ -std=c++11 -O3 -I"C:/ilionData/Users/zeno.pelgrims/Desktop/Arnold-4.2.12.0-windows/include" -L"C:/ilionData/Users/zeno.pelgrims/Desktop/Arnold-4.2.12.0-windows/bin" -lai --shared C:/ilionData/Users/zeno.pelgrims/Documents/lightfilter/src/lightColorAttenuation.cpp -o C:/ilionData/Users/zeno.pelgrims/Documents/lightfilter/bin/lightColorAttenuation.dll
-C:/ilionData/Users/zeno.pelgrims/Desktop/Arnold-4.2.13.4-windows/bin/kick -l "C:\ilionData\Users\zeno.pelgrims\Documents\lightfilter\bin" -set options.skip_license_check on -i "C:/ilionData/Users/zeno.pelgrims/Documents/lightfilter/lightfilter_own_02.ass" -g 2.2 -v 2 -l "C:/Program Files/Ilion/IlionMayaFramework/2015/modules/mtoa/1.2.7.2.2-4.2.13.6/shaders" -o "C:/ilionData/Users/zeno.pelgrims/Documents/lightfilter/lightColorAttenuation.exr" -dp
-*/
-
 
 AI_SHADER_NODE_EXPORT_METHODS(dustMethods);
 
 
 enum dustParams{
     //p_mode,
-    //p_colorStart,
 };
 
 
 
 node_parameters {
     //AiParameterBOOL("mode", true);
-    //AiParameterRGB("colorStart", 1.0, 1.0, 1.0);
 }
 
 node_initialize {
@@ -64,8 +55,6 @@ shader_evaluate{
     } else {
         sg->out.RGB = AI_RGB_BLACK;
     }
-
-
 
 }
 
